2450c49354a62c0ebdc4635d35a6b006f991ac96
Ivan Rodriguez <ivan.rodriguez@pix4d.com>
2019-08-07 15:59:30 +0200

    Removing C-style casts, and running clang-format over test_package

--- test_package/main.cpp
+++ test_package/main.cpp
@@ -1,65 +1,67 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #ifdef _WIN32
-# include <io.h>
-# include <fcntl.h>
+#include <fcntl.h>
+#include <io.h>
 #endif
 
 #include <zbar/zbar.h>
 
 #include <fstream>
 #include <iostream>
-#include <vector>
 #include <memory>
+#include <vector>
 
 namespace
 {
     std::unique_ptr<uint8_t[]> readBinary(const char* filename, unsigned long& rows, unsigned long& cols)
     {
         std::ifstream in(filename, std::ios::in | std::ios::binary);
-        in.read((char*)(&rows), sizeof(unsigned long));
-        in.read((char*)(&cols), sizeof(unsigned long));
+        in.read(reinterpret_cast<char*>(&rows), sizeof(unsigned long));
+        in.read(reinterpret_cast<char*>(&cols), sizeof(unsigned long));
 
         auto matrixBuffer = std::unique_ptr<uint8_t[]>(new uint8_t[rows * cols]);
-        in.read((char*)matrixBuffer.get(), rows * cols * sizeof(uint8_t));
+        in.read(reinterpret_cast<char*>(&matrixBuffer[0]), rows * cols * sizeof(uint8_t));
         in.close();
         return matrixBuffer;
     }
-}
+}  // namespace
 
-int main(int ac, char** av)    
+int main(int ac, char** av)
 {
     using namespace zbar;
     const std::vector<std::string> imagePaths(av + 1, av + ac);
 
-    auto processor = std::unique_ptr<zbar_processor_t, decltype(&zbar_processor_destroy)>(zbar_processor_create(0), zbar_processor_destroy);
-    if(zbar_processor_init(processor.get(), NULL, 0))
+    auto processor = std::unique_ptr<zbar_processor_t, decltype(&zbar_processor_destroy)>(zbar_processor_create(0),
+                                                                                          zbar_processor_destroy);
+    if (zbar_processor_init(processor.get(), NULL, 0))
     {
-        std::cout << "could not init processor " << std::endl;
+        std::cout << "Could not init processor " << std::endl;
     }
 
-    for(const auto& path : imagePaths)
-    {       
+    for (const auto& path : imagePaths)
+    {
         std::cout << "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" << std::endl;
-        std::cout << "Detection QR code for image " << path << std::endl;
+        std::cout << "Detecting QR code in image " << path << std::endl;
 
         unsigned long rows = 0, cols = 0;
         auto myMatrix = readBinary(path.c_str(), rows, cols);
 
-        auto zimage = std::unique_ptr<zbar_image_t, decltype(&zbar_image_destroy)>(zbar_image_create(), zbar_image_destroy);
+        auto zimage =
+            std::unique_ptr<zbar_image_t, decltype(&zbar_image_destroy)>(zbar_image_create(), zbar_image_destroy);
         zbar_image_set_size(zimage.get(), cols, rows);
-        zbar_image_set_format(zimage.get(), *(unsigned long*)"Y800");
+        zbar_image_set_format(zimage.get(), zbar_fourcc('Y', '8', '0', '0'));
 
         zbar_image_set_data(zimage.get(), &myMatrix[0], cols * rows, NULL);
         std::cout << "Cols " << cols << " Rows " << rows << std::endl;
 
         zbar_process_image(processor.get(), zimage.get());
 
-        const zbar_symbol_t *sym = zbar_image_first_symbol(zimage.get());
-        for(; sym; sym = zbar_symbol_next(sym))
+        const zbar_symbol_t* sym = zbar_image_first_symbol(zimage.get());
+        for (; sym; sym = zbar_symbol_next(sym))
         {
             zbar_symbol_type_t typ = zbar_symbol_get_type(sym);
-            if(typ == ZBAR_QRCODE)
+            if (typ == ZBAR_QRCODE)
             {
                 const char* data = zbar_symbol_get_data(sym);
                 std::cout << "QR code text: " << data << std::endl;
