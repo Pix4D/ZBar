51fa99669b3b33896f9ec55869207a090c2bbf3b
Benedikt Führer <benedikt.fuehrer@pix4d.com>
2020-12-11 19:14:53 +0100

    Fix test_package (unsigned long has different byte sizes between Linux/Win x64) and make it fail when the QR-code isn't recognized correctly

--- test_package/main.cpp
+++ test_package/main.cpp
@@ -15,11 +15,11 @@
 
 namespace
 {
-    std::unique_ptr<uint8_t[]> readBinary(const char* filename, unsigned long& rows, unsigned long& cols)
+    std::unique_ptr<uint8_t[]> readBinary(const char* filename, uint64_t& rows, uint64_t& cols)
     {
         std::ifstream in(filename, std::ios::in | std::ios::binary);
-        in.read(reinterpret_cast<char*>(&rows), sizeof(unsigned long));
-        in.read(reinterpret_cast<char*>(&cols), sizeof(unsigned long));
+        in.read(reinterpret_cast<char*>(&rows), sizeof(uint64_t));
+        in.read(reinterpret_cast<char*>(&cols), sizeof(uint64_t));
 
         std::unique_ptr<uint8_t[]> matrixBuffer{ new uint8_t[rows * cols] };
         in.read(reinterpret_cast<char*>(&matrixBuffer[0]), rows * cols * sizeof(uint8_t));
@@ -33,7 +33,7 @@ int main(int ac, char** av)
     const std::vector<std::string> imagePaths(av + 1, av + ac);
 
     auto processor = std::unique_ptr<zbar_processor_t, decltype(&zbar_processor_destroy)>(zbar_processor_create(0),
-        zbar_processor_destroy);
+                                                                                          zbar_processor_destroy);
     if (zbar_processor_init(processor.get(), NULL, 0))
     {
         std::cout << "Could not init processor " << std::endl;
@@ -44,7 +44,7 @@ int main(int ac, char** av)
         std::cout << "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" << std::endl;
         std::cout << "Detecting QR code in image " << path << std::endl;
 
-        unsigned long rows = 0, cols = 0;
+        uint64_t rows = 0, cols = 0;
         auto myMatrix = readBinary(path.c_str(), rows, cols);
 
         auto zimage =
@@ -65,7 +65,10 @@ int main(int ac, char** av)
             {
                 const char* data = zbar_symbol_get_data(sym);
                 std::cout << "QR code text: " << data << std::endl;
+                return 0;
             }
         }
     }
+
+    return 1;
 }
